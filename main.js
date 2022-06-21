51755065291c006ade9861616f493f78afc38058
hipolito12 <84524423+hipolito12@users.noreply.github.com>
2022-06-21 02:31:56 -0300

    Animations

--- main.js
+++ main.js
@@ -1,55 +1,74 @@
-// document.addEventListener( "mousemove", function(e)
-// {
-//     var body= document.querySelector ('body');
-//     document.createElement ("span");
-//     var heart;
-//     var x= e.offsetX;
-//     var y =e.offsetY;
-//     heart.style.left = x+"px";
-//     heart.style.top= y+"px";
-//     var size =Math.random()*100;
-//     heart.style.width =20+size +'px';
-//     heart.style.height =20+size+"px";
-//     body.appendChild(heart);
-//     setTimeout (function () {
-//     heart.remove () ;
-//     }, 2000);
-
-// });
-function AnimateElements() {}
 var slideup = document.getElementById("slideup");
 var neont = document.getElementById("neon_title");
 var boton = document.getElementById("boton_en_corazon");
 var mage1 = document.getElementById("image1");
 var mostrar = document.getElementById("ocultar");
 var img2 = document.getElementById("box2");
-
+var image2 = document.getElementById("image2");
+var vegetta = document.getElementById("vegetta");
+var goku = document.getElementById("sexo");
+var defo1 = document.getElementById("img2");
+var monacontainer = document.getElementById("monacontainer");
+var music = document.getElementById("musica");
+var bailes= document.getElementById("bailes");
 boton.addEventListener("click", function (e) {
   e.preventDefault();
   neont.style.visibility = "visible";
-  
-  setTimeout(function () {
-    image1.classList.add("animate__flipInY");
-    image1.style.visibility = "visible";
+  image1.classList.add("animate__flipInX");
+  image1.style.setProperty("animation-duration", "2s");
+  image1.style.visibility = "visible";
+  image2.classList.add("animate__flipInX");
+  image2.style.setProperty("animation-duration", "2s");
+  image2.style.visibility = "visible";
 
-    image1.style.setProperty("--animate-duration", "1s");
-    let image2 = document.getElementById("image2");
-    image2.classList.add("animate__flipInY");
-    image2.style.visibility = "visible";
-    
-   
-    mostrar.style.visibility = "visible";
-    
-    
-  }, 150);
+  mostrar.style.visibility = "visible";
+
   setTimeout(function () {
     slideup.style.visibility = "visible";
     boton.style.visibility = "hidden";
     slideup.classList.add("animate__fadeInUpBig");
     slideup.style.visibility = "visible";
+    slideup.style.setProperty("--animate-duration", "1s");
+  }, 600);
+  setTimeout(function () {
+    monacontainer.style.visibility = "visible";
+  }, 1000);
 
-    slideup.style.setProperty("--animate-duration", "0.05s");
-  }, 200);
+  setTimeout(function () {
+    vegetta.classList.add("animate__bounceOutRight");
+    vegetta.style.setProperty("--animate-duration", "8s");
+    vegetta.style.visibility = "visible";
+    music.innerHTML="<audio  src='ner.mp3' autoplay ></audio>";
+    setTimeout(function () {
+      bailes.classList.add("animate__fadeOutUpBig");
+      bailes.style.setProperty("--animate-duration", "180s");
+      bailes.style.visibility = "visible";
+
+    }, 3000);
+  }, 3000);
+  setTimeout(function () {
+    goku.classList.add("animate__bounceOutLeft");
+    goku.style.setProperty("--animate-duration", "9s");
+    goku.style.visibility = "visible";
+  }, 6000);
 });
 
+img2.addEventListener("click", function (e) {
+  img2.classList.add("animate__rotateIn");
+  img2.style.setProperty("--animate-duration", "1s");
 
+  setTimeout(() => {
+    img2.classList.remove("animate__rotateIn");
+  }, 1000);
+
+});
+
+mage1.addEventListener("click", function (e) {
+
+  mage1.classList.add("animate__rotateIn");
+  mage1.style.setProperty("--animate-duration", "1s");
+  setTimeout(() => {
+    mage1.classList.remove("animate__rotateIn");
+  }, 1000);
+
+});
